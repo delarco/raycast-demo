d9e1c7ac5d2baa77a18c13b0a0329ffe2a36d3fc
Leandro Del Arco <leandrodelarco@outlook.com>
2024-03-01 01:04:18 -0300

    update main to start game

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './style.css'
+import { Game, GameConfig } from 'raycast-ts'
+import { StartScene } from './Scenes/StartScene'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+const config = new GameConfig(app)
+const game = new Game(config)
+
+game.start(StartScene)
