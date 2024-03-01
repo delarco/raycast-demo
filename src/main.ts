06dbd44e036bbbf7cbb36adc4dbc411e8c4c5bbb
Leandro Del Arco <leandrodelarco@outlook.com>
2024-03-01 01:15:16 -0300

    add loading element

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,9 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 const config = new GameConfig(app)
 const game = new Game(config)
 
+// setup loading
+const loadingSpan = document.querySelector<HTMLSpanElement>("#loading")!
+game.onLoadingStart = () => loadingSpan.style.visibility = "visible";
+game.onLoadingEnd = () => loadingSpan.style.visibility = "hidden";
+
 game.start(StartScene)
