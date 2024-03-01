dd17b3fbbcce006de75aff66cea5ef5fe5bfb28c
Leandro Del Arco <leandrodelarco@outlook.com>
2024-03-01 01:11:08 -0300

    create and implement Map2Scene

---
+++ src/Scenes/Map2Scene.ts
@@ -0,0 +1,64 @@
+import { AudioType, Camera, Color, RaycastScene, TextureUtils } from "raycast-ts";
+
+export class Map2Scene extends RaycastScene {
+
+    public ambientLight = 0.8
+    public distanceShadeFactor = 0.05
+    protected distanceShade = (distance: number): number => 1.0 - distance * this.distanceShadeFactor
+
+    public preload(): void {
+
+        this.load.mapFromIntArray([
+            1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1,
+            1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1,
+            1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1,
+            1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1,
+            1, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ],
+            {
+                width: 20,
+                height: 20
+            },
+            {
+                1: Color.WHITE,
+                2: Color.LIGHT_BLUE,
+                3: Color.BLUE,
+                4: Color.ORANGE,
+                5: Color.RED,
+            }
+        )
+
+        this.load.texture("FOUNTAIN", "./images/fountain.png")
+        this.load.audio("MUSIC", AudioType.MUSIC, "./audios/medieval-chateau.mp3")
+    }
+
+    public init(): void {
+
+        this.camera = new Camera(9.5, 18, 3 * Math.PI / 2)
+        super.init()
+
+        this.map.skybox = TextureUtils.makeSkyBoxDayTexture(this.gameInstance.resolution.height)
+        this.map.floor = Color.DARK_GREEN
+
+        this.add.sprite(10, 1.5, 1, 1, "FOUNTAIN", { frameWidth: 160 })
+
+        const music = this.load.getAudio("MUSIC")!
+        music.volume = 0.4
+        music.play()
+    }
+}
